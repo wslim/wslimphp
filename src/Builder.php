67d345e1f375fc2dbecd2d6ee76826c68fcfae6a
wslim <gwbnet@126.com>
2018-08-03 00:57:56 +0800

    create 2.0.0

---
+++ src/Builder.php
@@ -0,0 +1,19 @@
+<?php
+namespace Wslim;
+
+/**
+ * builder
+ * 
+ * build app(web), module, controller, model, view
+ * 
+ * @author 28136957@qq.com
+ * @link   wslim.cn
+ */
+class Builder
+{
+    static public function build()
+    {
+        echo 'build app';
+    }
+    
+}
